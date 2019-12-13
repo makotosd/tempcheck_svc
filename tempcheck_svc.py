4f0fe4fb0b2bbc20bf67f6eb4c143222679c4123
makotosd <mouri.makoto@gmail.com>
2019-12-14 00:24:26 +0900

    wrong internal port number

--- tempcheck_svc.py
+++ tempcheck_svc.py
@@ -29,7 +29,7 @@ def check_temp():
     if response_json is None:
         # 温度履歴を取得
         response = requests.get(
-            'http://m2x:8081/m2x_temperature'
+            'http://m2x:8080/m2x_temperature'
         )
         response_json = response.json()
 
