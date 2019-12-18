76f8126780a0d8b0bac77e41ad7814e83c19a28c
mm <mmm@zzz.com>
2019-12-18 23:09:46 +0900

    ステートを持たせた。

--- tempcheck_svc.py
+++ tempcheck_svc.py
@@ -2,7 +2,6 @@
 import requests
 from flask import Flask, jsonify, request
 import datetime
-import dateutil.parser
 from enum import Enum
 
 
@@ -12,11 +11,15 @@ class TempState(Enum):
     HIGH = 3
 
 
-LOW_TEMP = 20
-HIGH_TEMP = 35
-MSG_LOW = "Temperature is too LOW. "
-MSG_HIGH = "Temperature is too HIGH. "
-MSG_NOM = "Temperature return to NORMAL. "
+LOW_TEMP = 20.0
+HIGH_TEMP = 35.0
+DELTA_TEMP = 1.0
+MSG_LOW = "じぇじぇじぇ、ちょっとすずしいなぁー. "
+MSG_HIGH = "じぇ、あちっちー. "
+MSG_NOM = "ふぅ、かいてきかいてき。ぴよ。"
+preStatus = None
+preStatusStart = None
+preMsgTime = None
 
 app = Flask(__name__)
 
@@ -29,7 +32,8 @@ def check_temp():
     if response_json is None:
         # 温度履歴を取得
         response = requests.get(
-            'http://m2x:8080/m2x_temperature'
+            # 'http://m2x:8080/m2x_temperature'
+            'http://192.168.1.100:8081/m2x_temperature'
         )
         response_json = response.json()
 
@@ -45,89 +49,80 @@ def check_temp():
 
 # 温度履歴をもとに、メッセージを返す
 def message_gen(values):
-    now = datetime.datetime.now()
+    global preMsgTime
+    global preStatus
+    global preStatusStart
 
-    (f_last_value_is_edge, edge_message) = last_value_is_edge(values)
-    c_state = state_temp(float(values[0]['value']))
-    if f_last_value_is_edge is True:  # 状態遷移があった場合
-        edge_list = gen_edge_list(values)
-        old_edge = dateutil.parser.parse(edge_list[1]['timestamp'])
-        new_edge = dateutil.parser.parse(edge_list[0]['timestamp'])
-        edge_duration = new_edge - old_edge
-        if edge_duration > datetime.timedelta(hours=1):  # 前回の状態遷移から1時間以上空いていた場合
-            return edge_message
-    elif c_state is not TempState.NORMAL:       # 異常状態にいる場合
-        duration = wrong_range_duration(values)
-        onehour = datetime.timedelta(hours=1)
-        if (duration % onehour) < datetime.timedelta(minutes=8) or \
-                (duration % onehour) > datetime.timedelta(minutes=52):  #  60分プラスマイナス8分
-            if c_state is TempState.LOW:
-                return MSG_LOW + "{:.2f}C".format(float(values[0]['value']))
-            elif c_state is TempState.HIGH:
-                return MSG_HIGH + "{:.2f}C".format(float(values[0]['value']))
-            else:
-                return "ERROR: 2000"
-    else:
-        return None
+    c_temp = float(values[0]['value'])
+    c_status = state_temp2(c_temp, preStatus)
 
-# 不正な状態にどれだけ継続しているか？
-#
-def wrong_range_duration(values):
-    now = dateutil.parser.parse(values[0]['timestamp'])
-    for v in values:
-        state = state_temp(float(v['value']))
-        if state is TempState.NORMAL:
-            return now - dateutil.parser.parse(v['timestamp'])
+    msg = None
 
-# 温度状態に関する標準関数軍
-def low_temp(temp):
-    return temp < LOW_TEMP
-def high_temp(temp):
-    return temp > HIGH_TEMP
-def normal_temp(temp):
-    return LOW_TEMP <= temp and temp <= HIGH_TEMP
-def state_temp(temp):
-    if temp < LOW_TEMP:
-        return TempState.LOW
-    elif temp > HIGH_TEMP:
-        return TempState.HIGH
-    else:
-        return TempState.NORMAL
+    if preStatus is None:
+        preStatus is c_status
+        preStatusStart = datetime.datetime.now()
+        preMsgTime = datetime.datetime.now()
+        msg = "ぴよ！げんき？"
 
+    elif preStatus is not c_status:  # 状態遷移あり
+        msg = get_msg(c_status, c_temp)
+        preStatus = c_status
+        preStatusStart = datetime.datetime.now()
+        preMsgTime = None
 
-# 最新のデータで状態が変化したか？
-# 一個前の状態変化はいつか？
-# 状態変化に対するメッセージ。
-def last_value_is_edge(values):
-    c_state = state_temp(values[0]['value'])
-    p_state = state_temp(values[1]['value'])
-
-    if c_state is p_state:
-        return False, None
-    else:
-        if c_state is TempState.HIGH:
-            return True, MSG_HIGH + "{:.2f}C".format(float(values[0]['value']))
-        elif c_state is TempState.LOW:
-            return True, MSG_LOW + '{:.2f}C'.format(float(values[0]['value']))
-        else:
-            return True, MSG_NOM + "{:.2f}C".format(float(values[0]['value']))
-
-
-def gen_edge_list(values):
-    edge_list = []
-    previous_temp = None
-    for v in values:
-        current_temp = float(v['value'])  # 今の温度
-        if previous_temp is not None:
-            p_state = state_temp(previous_temp)  # 前の状態
-            c_state = state_temp(current_temp)   # 今の状態
-            if p_state is not c_state:  # 前の状態と今の状態が違っていたら
-                edge_list.append(v)   # いつのだったのか記録
+    else:  # 状態遷移なし
+        now = datetime.datetime.now()
+        if now - preStatusStart > datetime.timedelta(hours=1.0):
+            if now - preMsgTime > datetime.deltatime(hours=1.0):
+                preMsgTime = now
+                msg = get_msg(c_status, c_temp)
             else:
                 pass
-        previous_temp = current_temp
+        else:
+            pass
 
-    return edge_list
+    preStatus = c_status
+
+    return msg
+
+
+def get_msg(state, temp):
+    if state is TempState.HIGH:
+        msg = MSG_HIGH + ' {:.2f}C'.format(temp)
+    elif state is TempState.LOW:
+        msg = MSG_LOW + ' {:.2f}C'.format(temp)
+    else:
+        msg = MSG_NOM + ' {:.2f}C'.format(temp)
+    return msg
+
+
+# 温度状態に関する標準関数軍
+def state_temp2(t, p_state):
+    if p_state is None:
+        if t < LOW_TEMP:
+            return TempState.LOW
+        elif t > HIGH_TEMP:
+            return TempState.HIGH
+        else:
+            return TempState.NORMAL
+    else:
+        if p_state is TempState.LOW:
+            if t < LOW_TEMP + DELTA_TEMP:
+                return TempState.LOW
+            else:
+                return TempState.NORMAL
+        if p_state is TempState.NORMAL:
+            if t < LOW_TEMP:
+                return TempState.LOW
+            elif t > HIGH_TEMP:
+                return TempState.HIGH
+            else:
+                return TempState.NORMAL
+        if p_state is TempState.HIGH:
+            if t > HIGH_TEMP - DELTA_TEMP:
+                return TempState.HIGH
+            else:
+                return TempState.NORMAL
 
 
 if __name__ == '__main__':
